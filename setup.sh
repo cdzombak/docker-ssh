84d7681b56a77fef78a98dc27ae0ebe27e930e59
Chris Dzombak <chris@dzombak.com>
2023-09-06 13:17:24 -0400

    add example for creating a user at build time

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# One-time setup at build time.
+
+# NEWUSERNAME=anewusername
+# NEWUSERPASS=foobarbaz123
+
+# useradd -d /a-shared-directory/ -u 1000 -g users "$NEWUSERNAME"
+# echo "$NEWUSERNAME:$NEWUSERPASS" | chpasswd
